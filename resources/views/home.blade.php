c44692a6e55ae24446d3cc383687b1eb3925cf80
Pablo Braz de Araújo <pbraz0460@gmail.com>
2025-05-23 08:14:55 -0300

    teste do baner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,54 +30,24 @@
         </nav>
     </section>
 
-    <!-- Seção do Carrossel -->
-    <section class="section-carrossel">
-        <div id="carouselHome" class="carousel slide h-100" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselHome" data-bs-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselHome" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselHome" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner h-100">
-                <div class="carousel-item active h-100">
-                    <img src="{{ asset('img/Mitsubishi-Outlander-Sport-2021-Foto-Leo-Sposito-7.jpg') }}"
-                        class="d-block w-100 h-100 banner-img" alt="Mitsubishi Outlander">
+    <!-- Seção do card -->
+    <section class="section-card">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="/public/img/teste.png" class="img-fluid rounded-start animate-kenburns" alt="...">
                 </div>
-                <div class="carousel-item h-100">
-                    <img src="{{ asset('img/Volkswagen-Novo-Polo.jpg') }}" class="d-block w-100 h-100 banner-img"
-                        alt="Volkswagen Polo">
-                </div>
-                <div class="carousel-item h-100">
-                    <img src="{{ asset('img/OIP.png') }}" class="d-block w-100 h-100 banner-img" alt="OIP">
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    </div>
                 </div>
             </div>
-            <button class="carousel-control-prev custom-carousel-arrow" type="button" data-bs-target="#carouselHome"
-                data-bs-slide="prev">
-                <span aria-hidden="true">
-                    <svg width="32" height="32" fill="#fff" viewBox="0 0 24 24">
-                        <circle cx="12" cy="12" r="12" fill="rgba(124,58,237,0.7)" />
-                        <path d="M15.5 19 9.5 12 15.5 5" stroke="#fff" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" fill="none" />
-                    </svg>
-                </span>
-                <span class="visually-hidden">Anterior</span>
-            </button>
-            <button class="carousel-control-next custom-carousel-arrow" type="button" data-bs-target="#carouselHome"
-                data-bs-slide="next">
-                <span aria-hidden="true">
-                    <svg width="32" height="32" fill="#fff" viewBox="0 0 24 24">
-                        <circle cx="12" cy="12" r="12" fill="rgba(124,58,237,0.7)" />
-                        <path d="M8.5 5 14.5 12 8.5 19" stroke="#fff" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" fill="none" />
-                    </svg>
-                </span>
-                <span class="visually-hidden">Próximo</span>
-            </button>
         </div>
     </section>
+    <!-- Seção do card -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
