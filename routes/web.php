538c2d3b034bfce665cc0cf746bd1380600f50e9
pablogfgdgf <pbraz0460@gmail.com>
2025-07-03 22:23:28 -0300

    Login
    o sistema esta salvando login , mas não permite acessar as rotas de cadastrar e logar dps que o usuario ja esta ogado arrumar dps

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,8 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-// ROTAS DE CADASTRO E LOGIN REMOVIDAS - Já existem em seus próprios arquivos
 
-// ROTA DE LOGOUT REMOVIDA - Já existe em routes/auth.php
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
